233b7baee3a72cdeada85c55d34e62fb16ac29df
Mikkel Katholm <53199185+MikkelKatholm@users.noreply.github.com>
2025-10-27 13:50:51 +0100

    Did stuff that mostly work

---
+++ Coding/mp-spdz-0.4.1/Scripts/decompile.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+sys.path.append('.')
+
+from Compiler.instructions_base import Instruction
+from Compiler.program import *
+
+if len(sys.argv) <= 1:
+    print('Usage: %s <program>' % sys.argv[0])
+
+for tapename in Program.read_tapes(sys.argv[1]):
+    filename = 'Programs/Bytecode/%s.asm' % tapename
+    print('Creating', filename)
+    with open(filename, 'w') as out:
+        for i, inst in enumerate(Tape.read_instructions(tapename)):
+            print(inst, '#', i, file=out)

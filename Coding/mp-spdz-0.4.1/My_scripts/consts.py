20cfb1b833b7b4c31d277f7171beaa15c992b9e8
Mikkel Katholm <53199185+MikkelKatholm@users.noreply.github.com>
2025-11-12 12:49:58 +0100

    minor stuff

--- Coding/mp-spdz-0.4.1/My_scripts/consts.py
+++ Coding/mp-spdz-0.4.1/My_scripts/consts.py
@@ -4,7 +4,7 @@ NUM_PARTIES = 3
 NUM_VOTES = 5
 
 ## For shamir
-#FIELD_SIZE = 2**127 - 1  # A large prime number for the finite field
-FIELD_SIZE = 1613
+FIELD_SIZE = 2**31 - 1  # To use with numbers larger then 2^64 the program must be compiled with the flag -F {field_size in bits}
+#FIELD_SIZE = 1613
 NUM_SHARES = NUM_PARTIES
 THRESHOLD = NUM_PARTIES

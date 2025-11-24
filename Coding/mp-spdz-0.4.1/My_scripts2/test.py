5f6f92993341be2544663c79d694af2c08f1f66b
Mikkel Katholm <53199185+MikkelKatholm@users.noreply.github.com>
2025-11-24 14:56:35 +0100

    model with more leakage now implemented

--- Coding/mp-spdz-0.4.1/My_scripts2/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-client_id = int(sys.argv[1])
-
-with open(f"My_scripts2/client_inputs/{client_id}_client") as f:
-    client_input =[int(x) for x in f.read().split()]
-
-print("Client", client_id, "input:", client_input)

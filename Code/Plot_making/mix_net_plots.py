1c4889f0d4beb018b3a4a702571e05612ff95e7f
Emil Mors <au705768@uni.au.dk>
2026-04-23 12:11:33 +0200

    Auto commit on 2026-04-23 12:11:33

--- Code/Plot_making/mix_net_plots.py
+++ Code/Plot_making/mix_net_plots.py
@@ -12,13 +12,70 @@ def plot_varying_clients():
         (data['NUM_CLIENTS'] != 32)
     ]
 
-    grouped = filtered.groupby('NUM_CLIENTS')['time_total'].mean().reset_index()
+    filtered['time_no_encrypt'] = filtered['time_total'] - filtered['t_encrypt_ballots']
+
+    grouped = (
+        filtered
+        .groupby('NUM_CLIENTS')['time_no_encrypt']
+        .mean()
+        .reset_index()
+    )
 
     plt.figure()
-    plt.plot(grouped['NUM_CLIENTS'], grouped['time_total'], marker='o')
+    plt.plot(grouped['NUM_CLIENTS'], grouped['time_no_encrypt'], marker='o')
     plt.grid()
     plt.xlabel('Number of Clients')
-    plt.ylabel('Total Time (s)')
-    plt.show()
+    plt.ylabel('Tallying Time (s)')
+    #plt.title('Tallying time vs Number of Clients')
+    plt.savefig('mix_net_varying_clients_plot.pdf', bbox_inches='tight')
 
+def plot_varying_servers():
+    filtered = data[
+        (data['NUM_CLIENTS'] == 32) &
+        (data['NUM_CANDS'] == 5)
+    ]
+
+    filtered['time_no_encrypt'] = filtered['time_total'] - filtered['t_encrypt_ballots']
+
+    grouped = (
+        filtered
+        .groupby('NUM_SERVERS')['time_no_encrypt']
+        .mean()
+        .reset_index()
+    )
+
+    plt.figure()
+    plt.plot(grouped['NUM_SERVERS'], grouped['time_no_encrypt'], marker='o')
+    plt.grid()
+    plt.xlabel('Number of Servers')
+    plt.ylabel('Tallying time Time (s)')
+    #plt.title('Tallying time vs Number of Servers')
+    plt.savefig('mix_net_varying_servers_plot.pdf', bbox_inches='tight')
+
+def plot_varying_candidates():
+    filtered = data[
+        (data['NUM_CLIENTS'] == 32) &
+        (data['NUM_SERVERS'] == 5)
+    ]
+
+    filtered['time_no_encrypt'] = filtered['time_total'] - filtered['t_encrypt_ballots']
+
+    grouped = (
+        filtered
+        .groupby('NUM_CANDS')['time_no_encrypt']
+        .mean()
+        .reset_index()
+    )
+
+    plt.figure()
+    plt.plot(grouped['NUM_CANDS'], grouped['time_no_encrypt'], marker='o')
+    plt.ylim(bottom=grouped['time_no_encrypt'].min() * 0.9, top=grouped['time_no_encrypt'].max() * 1.1)
+    plt.grid()
+    plt.xlabel('Number of Candidates')
+    plt.ylabel('Tallying time Time (s)')
+    #plt.title('Tallying time vs Number of Candidates')
+    plt.savefig('mix_net_varying_candidates_plot.pdf', bbox_inches='tight')
+
+plot_varying_candidates()
 plot_varying_clients()
+plot_varying_servers()
